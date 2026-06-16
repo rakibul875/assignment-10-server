fd6296f696934e2894a517ced8f56bfad6e94b6f
rakibul875 <rakibulislamashik78@gmail.com>
2026-06-16 21:08:40 +0600

    connected mongodb

--- index.js
+++ index.js
@@ -1,6 +1,48 @@
 const express = require('express');
 const app = express()
-const port = 3000
+const dotenv = require("dotenv");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const port = 8000
+dotenv.config();
+const uri=process.env.MONGODB_URI;
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+app.use(cors());
+app.use(express.json());
+
+async function run() {
+  try {
+    await client.connect();
+    const database = client.db("assignment-10");
+    const usersCollection = database.collection("user");
+
+
+
+    app.get("/users", async (req, res) => {
+      const cursor = usersCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
